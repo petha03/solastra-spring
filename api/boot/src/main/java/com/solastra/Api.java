2eeaa49d0354a5ea5b0e46d1fb3b99dd4181144a
Hans Petersen <30960661+petha03@users.noreply.github.com>
2025-10-12 15:40:16 -0600

    Refactored existing lambda function using spring to use spring boot with rest controllers instead. (#1)

--- api/boot/src/main/java/com/solastra/Api.java
+++ api/boot/src/main/java/com/solastra/Api.java
@@ -1,38 +1,13 @@
 package com.solastra;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Api {
+public class Api extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Api.class, args);
-    }
-
-    @Bean
-    public Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> handleRequest() {
-
-        return input -> {
-
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/json");
-            headers.put("Access-Control-Allow-Origin", "*");
-
-            String body = input.getBody();
-            String response = "Echo: " + body;
-
-            return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(200)
-                    .withHeaders(headers)
-                    .withBody("\"" + response + "\"");
-        };
     }
 }
