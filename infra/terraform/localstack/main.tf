2bbba7f4cec94f896f299cc83cfb4c8cad5b73c1
Hans Petersen <30960661+petha03@users.noreply.github.com>
2025-10-16 18:00:35 -0600

    Added application directory with a vue single page web app that is put in localstack and is served from s3. (#3)

--- infra/terraform/localstack/main.tf
+++ infra/terraform/localstack/main.tf
@@ -97,3 +97,56 @@ resource "aws_lambda_permission" "apigw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.solastra_api.execution_arn}/*/*"
 }
+
+# S3 bucket for Vue application
+resource "aws_s3_bucket" "vue_app" {
+  bucket = "solastra-vue-app"
+}
+
+resource "aws_s3_bucket_website_configuration" "vue_app" {
+  bucket = aws_s3_bucket.vue_app.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "vue_app" {
+  bucket = aws_s3_bucket.vue_app.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "vue_app" {
+  bucket = aws_s3_bucket.vue_app.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.vue_app.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.vue_app]
+}
+
+output "api_endpoint" {
+  value = "${aws_api_gateway_stage.dev.invoke_url}"
+}
+
+output "vue_app_url" {
+  value = "http://${aws_s3_bucket.vue_app.bucket}.s3-website.localhost.localstack.cloud:4566"
+}
